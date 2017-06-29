c099f42f85ea110bef4887e8370aee23654e8241
Kylin <kylin@192.168.1.3>
2017-06-29 22:51:24 +0800

    alert page

--- src/app/charts/lineCharts/monitor.component.ts
+++ src/app/charts/lineCharts/monitor.component.ts
@@ -5,73 +5,61 @@ import { NgClass } from '@angular/common';
 
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 
+import { TableData } from '../../dashboard/table-data-alart';
+
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-charts',
   templateUrl: './monitor.component.html',
 })
 export class MonitorComponent implements OnInit {
-
+  date: any;
   constructor(
     private parent: NavComponent,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
+  ) {
+    this.date = new Date().getDate();;
+    this.date = moment(new Date()).format('YYYY/MM/DD');
+  }
 
   ngOnInit() {
     this.parent.setActiveByPath("charts", this.parent.monitor);
   };
 
-
-  // lineChart
-  public lineChartData: Array<any> = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    { data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C' }
+  public category_list: Array<any> = [
+    { 'id': '办公软件', 'text': '办公软件' },
+    { 'id': '图形图象/视频', 'text': '图形图象/视频' },
+    { 'id': '编程语言与程序设计', 'text': '编程语言与程序设计' },
+    { 'id': '数据库/算法', 'text': '数据库/算法' },
+    { 'id': '操作系统/移动开发', 'text': '操作系统/移动开发' },
+    { 'id': '网站设计/网络技术', 'text': '网站设计/网络技术' },
+    { 'id': 'AutoCAD/辅助设计', 'text': 'AutoCAD/辅助设计' },
+    { 'id': '硬件/维护', 'text': '硬件/维护' },
+    { 'id': '计算机理论', 'text': '计算机理论' },
+    { 'id': 'IT人文', 'text': 'IT人文' },
   ];
-  public lineChartLabels: Array<any> = ['类别', '店铺', 'ISBN', '指标', '值', '日期'];
-  public lineChartOptions: any = {
-    animation: false,
-    responsive: true
-  };
-  public lineChartColours: Array<any> = [
-    { // green
-      backgroundColor: 'rgba(202,252,209,0.2)',
-      borderColor: 'rgba(90,202,106,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // gre
-      backgroundColor: 'rgba(168,226,178,0.2)',
-      borderColor: 'rgba(5,124,22,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
+  test_value = TableData;
+  test_label = [
+    { title: '店铺', name: 'shop', sort: '' },
+    { title: '书目', name: 'price', sort: 'asc' },
+    { title: '公式', name: 'discount', sort: 'asc' },
+    { title: '类型', name: 'sale', sort: 'asc' },
+    { title: '值', name: 'comment', sort: 'asc' },
+    { title: '日期', name: 'inv', sort: 'asc' }
   ];
-  public lineChartLegend: boolean = true;
-  public lineChartType: string = 'line';
-
-  public randomize(): void {
-    let _lineChartData: Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = { data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label };
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-    this.lineChartData = _lineChartData;
-  }
+  public period_list: Array<any> = [
+    { 'id': '3d', 'text': '3天' },
+    { 'id': '1w', 'text': '1周' },
+    { 'id': '2w', 'text': '2周' },
+    { 'id': '1m', 'text': '1月' },
+  ];
+  public formula_list: Array<any> = [
+    { 'id': '1', 'text': '公式一' },
+    { 'id': '2', 'text': '公式二' },
+    { 'id': '3', 'text': '公式三' },
+  ];
 
   // events
   public chartClicked(e: any): void {
---
+++ src/app/dashboard/table-data-alart.ts
@@ -0,0 +1,38 @@
+export const TableData: Array<any> = [
+  {
+    'shop': '人邮',
+    'price': '软件工程',
+    'discount': 5,
+    'sale': '销量',
+    'comment': 99999,
+    'inv': '2017/06/29'
+  }, {
+    'shop': '当当',
+    'price': '操作系统',
+    'discount': 3,
+    'sale': '折扣',
+    'comment': 465789,
+    'inv': '2017/06/29'
+  }, {
+    'shop': '文轩',
+    'price': '计算机网络',
+    'discount': 1,
+    'sale': '售价',
+    'comment': 432,
+    'inv': '2017/06/29'
+  }, {
+    'shop': '机械',
+    'price': '数据结构',
+    'discount': 7,
+    'sale': '评论数',
+    'comment': 90879,
+    'inv': '2017/06/29'
+  }, {
+    'shop': '清华',
+    'price': 'angular',
+    'discount': 9,
+    'sale': '直通车投入',
+    'comment': 324,
+    'inv': '2017/06/29'
+  }
+];
