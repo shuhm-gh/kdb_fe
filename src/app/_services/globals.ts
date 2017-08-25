6ee73f442bf23cb2d417fccb4b5cce47a381c794
Kylin <shuhm.gg@gmail.com>
2017-08-25 21:39:50 +0800

    backend port 8888 -> 9090

--- src/app/_services/globals.ts
+++ src/app/_services/globals.ts
@@ -1,2 +1,2 @@
-export const api_base_url = 'http://kylin-ux.com:8888';
-//export const api_base_url = 'http://localhost:8888';
+//export const api_base_url = 'http://kylin-ux.com:8888';
+export const api_base_url = 'http://localhost:9090';
